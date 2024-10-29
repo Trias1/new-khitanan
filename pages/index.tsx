e54887179bf86b515d0e19ae9ec126bee731e08d
trias <iaszaen@gmail.com>
2024-10-29 18:04:58 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Invitation from "../components/organisms/Invitation";
 import Maps from "../components/organisms/Maps";
 import Privasi from "../components/organisms/Privasi";
 import Footer from "../components/organisms/Footer";
+
 export default function Home() {
   const [isPlaying, setIsPlaying] = useState(true);
 
